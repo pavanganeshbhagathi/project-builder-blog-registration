d43d0752cbf06625ac26097f16ac46d7da941316
pavanganeshbhagathi <pavanganeshbhagathi554@gmail.com>
2020-05-14 11:05:27 +0530

    last

--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -11,6 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import businesslogic.RegistrationValidation;
+import model.User;
+
 
 
 @WebServlet(urlPatterns= {"/signup"})
@@ -31,11 +34,12 @@ public class SignUpController extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	/*	String email = request.getParameter("email"); 
+		String email = request.getParameter("email"); 
 		String password = request.getParameter("password"); 
 		String confirmPassword = request.getParameter("confirmPassword"); 
 		LocalDateTime date= LocalDateTime.now(); 
-		
+		RegistrationValidation checkUser = new RegistrationValidation();
+		checkUser.checkUserDetails(email, password, confirmPassword);
 
 		
 		if(checkUser.checkUserDetails(email, password, confirmPassword))
@@ -58,7 +62,7 @@ public class SignUpController extends HttpServlet {
 			rd.forward(request, response);
 		}
 		
-		*/
+		
 	}
 
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,1 +1,42 @@
 package model;
+
+import java.time.LocalDateTime;
+
+public class User
+{
+private	String email;
+ private String password;
+ private LocalDateTime date;
+ 
+ 
+ 
+public User(String email, String password, LocalDateTime date) {
+	super();
+	this.email = email;
+	this.password = password;
+	this.date = date;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPassword() {
+	return password;
+}
+@Override
+public String toString() {
+	return "User [email=" + email + ", password=" + password + ", date=" + date + "]";
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public LocalDateTime getDate() {
+	return date;
+}
+public void setDate(LocalDateTime date) {
+	this.date = date;
+}
+
+}
